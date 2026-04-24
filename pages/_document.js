df98cd9efbd777845f5cc28821c1919b812c1047
aliakarma <443059463@stu.iu.edu.sa>
2026-04-24 17:08:25 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,11 @@ export default function Document() {
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#000000" />
 
         {/* Performance Fix: preconnect for DNS before font requests */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
