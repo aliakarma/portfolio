6c94e5db159ff958896c09259261c6749335dcef
aliakarma <443059463@stu.iu.edu.sa>
2026-06-09 21:40:59 +0500

    Create site.js

---
+++ data/site.js
@@ -0,0 +1,7 @@
+export const SITE_URL = 'https://www.aliakarma.codes'
+export const SITE_NAME = 'Ali Akarma'
+export const SITE_SHORT_NAME = 'Akarma'
+
+export const PROFILE_IMAGE_URL = `${SITE_URL}/profile.jpg`
+export const OG_IMAGE_URL = `${SITE_URL}/og-image.png`
+
